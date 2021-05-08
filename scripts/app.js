3bd33eda34231fae57ff015685ddb9a5adb405ff
Asad Khan <asadkhan6192@gmail.com>
2021-05-08 11:54:42 +0500

    Add playRound function
    This functions contains the main logic for the game

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,36 @@
-function computerPlay(){
+function computerPlay() {
     let computerChoice = Math.floor(Math.random() * 3);
-    switch(computerChoice){
+    switch (computerChoice) {
         case 0:
-            return "Rock";
+            return "rock";
         case 1:
-            return "Paper";
+            return "paper";
         case 2:
-            return "Scissors";
+            return "scissors";
     }
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You Win! Rock crushes Sissors";
+    }
+    else if (playerSelection == "paper" && computerSelection == "rock") {
+        return "You Win! Paper covers Rock";
+    }
+    else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "You Win! Scissor cut Paper";
+    }
+    else if (playerSelection == "rock" && computerSelection == "paper") {
+        return "You Lose! Paper covers Rock";
+    }
+    else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "You Lose! Scissor cuts Paper";
+    }
+    else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "You Lose! Rock crushes Scissors";
+    }
+    else {
+        return "It's a tie!";
+    }
+}
